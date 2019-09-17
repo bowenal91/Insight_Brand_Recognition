2bf5a2255eaa41e4503a9f875c8005f06133d563
Alec Bowen <alecbowen@pop-os.localdomain>
2019-09-16 17:46:29 -0700

    changed some of the libraries

--- scripts/handlers.py
+++ scripts/handlers.py
@@ -1,10 +1,30 @@
+import numpy as np
 from PIL import Image
+from keras.preprocessing.image import ImageDataGenerator,array_to_img,img_to_array,load_img
+
+
+
+#Class to manage images and prepare them for training
 
 class Image_Handler:
-    def __init__(self, filename, filename2):
-        self.bg = Image.open(filename)
-        self.logo = Image.open(filename2)
-        self.pix = self.im.load()
-        self.size = self.im.size
-    def distort:
-        #Return a random distortion of the image
+    def __init__(self, filename, bg_size, logo_size):
+        self.bg_static = load_img(filename)
+        self.bg = load_img(filename)
+        self.bg = self.bg.resize(bg_size)
+        self.pix_bg = img_to_array(self.bg)
+        self.logo_size = logo_size
+        self.logo = None
+        self.logo_transformed = None
+
+    def add_logo(self,filename):
+        self.logo = load_img(filename)
+        self.logo = self.logo.resize(self.logo_size)
+
+    def transform_logo(self):
+        #Perform a single random transformation on the logo
+        #These parameters create bounds for the random transformation
+
+    def add_logo(self):
+        #Picks a random location to add on the logo image
+        #If the logo pixel at a given location is pure black, then we only use the background pixel value at that point
+
