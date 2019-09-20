79df56ba633de6654749f83dd7b87f9937a94a44
Alec Bowen <bowenal91@gmail.com>
2019-09-20 09:00:02 -0700

    updated files for use with GluonCV

--- scripts/generate_training.py
+++ scripts/generate_training.py
@@ -1,6 +1,29 @@
 from handlers import *
 import glob
 import sys
+import mxnet as mx
+
+def write_line(img_path, im_shape, boxes, ids, idx):
+    h, w, c = im_shape
+    # for header, we use minimal length 2, plus width and height
+    # with A: 4, B: 5, C: width, D: height
+    A = 4
+    B = 5
+    C = w
+    D = h
+    # concat id and bboxes
+    labels = np.hstack((ids.reshape(-1, 1), boxes)).astype('float')
+    # normalized bboxes (recommanded)
+    labels[:, (1, 3)] /= float(w)
+    labels[:, (2, 4)] /= float(h)
+    # flatten
+    labels = labels.flatten().tolist()
+    str_idx = [str(idx)]
+    str_header = [str(x) for x in [A, B, C, D]]
+    str_labels = [str(x) for x in labels]
+    str_path = [img_path]
+    line = '\t'.join(str_idx + str_header + str_labels + str_path) + '\n'
+    return line
 
 def generate_training_image(img_data,n_logo):
     for i in range(n_logo):
@@ -15,7 +38,7 @@ if __name__ == "__main__":
     logo_file = sys.argv[3]
     N = 200
     names = glob.glob(src_dir+"*")
-    with open("labels.txt","w") as out:
+    with open("labels.lst","w") as out:
         for i in range(N):
             print(i)
             j = np.random.randint(len(names))
@@ -27,8 +50,13 @@ if __name__ == "__main__":
 
             #im.print_label(label_dir+str(i)+".txt")
             im.bg.save(dest_dir+str(i)+".jpg")
-            out.write(dest_dir+str(i)+".jpg\t")
-            for box in im.label_list:
-                out.write("{},{},{},{}\t".format(box[0],box[1],box[2],box[3]))
-            out.write("\n")
+            img = mx.image.imread(dest_dir+str(i)+".jpg")
+            #out.write(dest_dir+str(i)+".jpg\t")
+            all_boxes = np.array(im.label_list)
+            all_ids = np.array([0]*len(im.label_list))
+            #print(all_boxes)
+            line = write_line(dest_dir+str(i)+".jpg",img.shape,all_boxes,all_ids,i)
+
+            out.write(line)
+            #out.write("0\n")
 
