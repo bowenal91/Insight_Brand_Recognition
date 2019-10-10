d268425483577d66a96c445b85a228e694af8113
Alec Bowen <bowenal91@gmail.com>
2019-10-10 15:12:55 -0700

    updated video analysis scripts

--- Multi_Class_Detector/Video/run_video_new.py
+++ Multi_Class_Detector/Video/run_video_new.py
@@ -5,6 +5,7 @@ import mxnet as mx
 import numpy as np
 import matplotlib.pyplot as plt
 
+threshold = 0.8
 
 classes = ['Visa','Powerade','Hyundai','Coke','Adidas']
 stats = np.zeros(len(classes))
@@ -16,7 +17,9 @@ cap = cv2.VideoCapture('soccer.mp4')
 #out = cv2.VideoWriter('detected.avi',cv2.VideoWriter_fourcc(*'DIVX'),15,size)
 axes = None
 NUM_FRAMES = 6000
+inc = 1.0/float(NUM_FRAMES)
 img_array = []
+running = np.zeros((NUM_FRAMES,len(classes)))
 for i in range(NUM_FRAMES):
     try:
         ret,frame = cap.read()
@@ -27,12 +30,15 @@ for i in range(NUM_FRAMES):
     rgb_nd = rgb_nd.as_in_context(mx.gpu(0))
     class_IDs,scores,bounding_boxes = net(rgb_nd)
     for j in range(len(scores[0])):
-        if scores[0][j] >= 0.8:
+        if scores[0][j] >= threshold:
             thisClass = int(class_IDs[0][j].asnumpy())
-            stats[thisClass] += 1.0
+            bb = bounding_boxes[0][j].asnumpy()
+            A = (bb[2]-bb[0])*(bb[3]-bb[1])
+            stats[thisClass] += A*inc
+            running[i][thisClass] += A*inc
         else:
             break
-    img = gcv.utils.viz.cv_plot_bbox(frame,bounding_boxes[0],scores[0],class_IDs[0],class_names=net.classes,thresh=0.8)
+    img = gcv.utils.viz.cv_plot_bbox(frame,bounding_boxes[0],scores[0],class_IDs[0],class_names=net.classes,thresh=threshold)
 
     height,width,layers = img.shape
     size = (width,height)
@@ -43,15 +49,34 @@ for i in range(NUM_FRAMES):
     #gcv.utils.viz.cv_plot_image(img)
     #cv2.waitKey(1)
 
+for i in range(1,NUM_FRAMES):
+    running[i] = running[i] + running[i-1]
+
 x = np.arange(len(classes))
 y = stats
+plt.figure()
+#plt.subplot(211)
 plt.bar(x,y,edgecolor='k',linewidth=2,color=['black','red','green','blue','yellow'])
 plt.tick_params(axis='both',which='major',labelsize=12)
 plt.xticks(x,classes,fontsize=8)
 plt.ylabel("Appearances",fontsize=14)
-plt.savefig("Plot.png")
+plt.savefig("Plot1.png")
 
-out = cv2.VideoWriter('detected.mp4',cv2.VideoWriter_fourcc(*'H264'),45,size)
+x2 = np.array(range(NUM_FRAMES))/45.0
+plt.figure()
+#plt.subplot(212)
+cs = ['black','red','green','blue','yellow']
+for i in range(len(classes)):
+    plt.plot(x2,running[:,i],color=cs[i],linewidth=2)
+
+plt.legend(classes)
+plt.xlim(0.0,float(NUM_FRAMES)/45.0)
+plt.tick_params(axis='both',which='major',labelsize=12)
+plt.xlabel("Time (seconds)",fontsize=14)
+plt.ylabel("Appearances")
+plt.savefig("Plot2.png")
+
+out = cv2.VideoWriter('detected.avi',cv2.VideoWriter_fourcc(*'DIVX'),45,size)
 
 for i in range(len(img_array)):
     out.write(img_array[i])
